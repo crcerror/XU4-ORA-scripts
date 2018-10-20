8fe5b2293230a5497a976927614969088970baae
cyperghost <crcerror@users.noreply.github.com>
2018-10-20 14:26:02 +0200

    Create key14.sh

---
+++ key14.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Make right button work
+# by cyperghost
+
+[ -z $1 ] && leftswitch=24 || leftswitch=$1
+
+echo $rightswitch > /sys/class/gpio/export
+echo in > /sys/class/gpio/gpio$rightswitch/direction
+
+until [ "$status" = "0" ]; do
+    sleep 0.5
+    status=$(cat /sys/class/gpio/gpio$rightswitch/value)
+    echo "Status: $status - GPIO: $rightswitch"
+done
+
+echo "I am done! Resetting GPIO $rightswitch now!"
+echo $rightswitch > /sys/class/gpio/unexport
